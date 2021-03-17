6cacb1baf6b19653ad3021de70507a54c83d72ea
Soukayna Bousfiha <soukabousfiha@gmail.com>
2021-03-17 09:42:15 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CatalogueService} from './catalogue.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ecom-web';
+export class AppComponent implements OnInit {
+  public categories ;
+
+
+  constructor(private catService :CatalogueService){}
+  ngOnInit(): void{
+    this.getCategories();
+  }
+
+  private getCategories() {
+      this.catService.getResource("/categories").subscribe(data=>{
+        this.categories=data;
+      },err=>{
+        console.log(err);
+      })
+  }
 }
